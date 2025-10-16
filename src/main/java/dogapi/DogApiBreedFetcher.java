5230042700a056ab2a5f6d591505644ab65d791d
Caleb Irwin <me@calebirwin.ca>
2025-10-16 16:02:02 -0400

    tasks 1-3

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -19,17 +19,39 @@ public class DogApiBreedFetcher implements BreedFetcher {
 
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
+     * 
      * @param breed the breed to fetch sub breeds for
      * @return list of sub breeds for the given breed
-     * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
+     * @throws BreedNotFoundException if the breed does not exist (or if the API
+     *                                call fails for any reason)
      */
     @Override
     public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        String url = "https://dog.ceo/api/breed/" + breed + "/list";
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new BreedNotFoundException(breed);
+            }
+
+            String resp = response.body().string();
+            JSONObject json = new JSONObject(resp);
+
+            String jsonStatus = json.getString("status");
+            if (!jsonStatus.equals("success")) {
+                throw new BreedNotFoundException(breed);
+            }
+
+            JSONArray subBreeds = json.getJSONArray("message");
+
+            List<String> result = new ArrayList<>();
+            subBreeds.forEach((obj) -> result.add(obj.toString()));
+            return result;
+        } catch (Exception e) {
+            throw new BreedNotFoundException(breed);
+        }
     }
 }
